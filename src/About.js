99f7b074ef24ca5c7a02eeafef0d537289bea294
Ben Paget <ben.paget@live.fr>
2020-11-24 11:40:05 +0100

    Context workshop init

---
+++ src/About.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <>
+      <div>About Bwok.Bowk</div>
+    </>
+  );
+};
+
+export default About;
---
+++ src/Welcome.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Welcome = () => {
+    return ( 
+        <>
+        <p>Welcome to Bwok.Bwok! 10000 customer served daily!</p>
+        </>
+     );
+}
+ 
+export default Welcome;
