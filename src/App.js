372c9d0c533f159f7d211228b5f95559ab2e41c8
claurennt <c.benedetti1989@gmail.com>
2022-09-12 15:12:28 +0200

    update packages

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 import React from "react";
-import Home from "./Home"
-import About from "./About"
-import Admin from "./Admin"
-import Signin from "./Signin"
-import Signup from "./Signup"
-import Logout from "./Logout"
-import unicornPic from "../src/pics/unicorns_inc.jpg"
-import { Link, Route, Switch } from "react-router-dom";
+import Home from "./Home";
+import About from "./About";
+import Admin from "./Admin";
+import Signin from "./Signin";
+import Signup from "./Signup";
+import Logout from "./Logout";
+import unicornPic from "../src/pics/unicorns_inc.jpg";
+import { Link, Route, Routes } from "react-router-dom";
 
 const App = () => {
-
   return (
     <div className="main-container">
       <img src={unicornPic} alt="Unicorns Inc" />
@@ -18,29 +17,28 @@ const App = () => {
           <Link to="/">Home</Link>
         </li>
         <li>
-          <Link to="/about">About</Link>
+          <Link to="about">About</Link>
         </li>
         <li>
-          <Link to="/admin">Admin</Link>
+          <Link to="admin">Admin</Link>
         </li>
         <li>
-          <Link to="/signin">Signin</Link>
+          <Link to="signin">Signin</Link>
         </li>
         <li>
-          <Link to="/signup">Signup</Link>
+          <Link to="signup">Signup</Link>
         </li>
       </ul>
-      <Switch>
-            <Route path="/about" component={About} />
-            <Route path="/admin" component={Admin} />
-            <Route path="/signin" component={Signin} />
-            <Route path="/signup" component={Signup} />
-            <Route exact path="/" component={Home} />
-      </Switch>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="admin" element={<Admin />} />
+        <Route path="signin" element={<Signin />} />
+        <Route path="signup" element={<Signup />} />
+      </Routes>
       {true && <Logout />}
     </div>
   );
 };
-
 
 export default App;
